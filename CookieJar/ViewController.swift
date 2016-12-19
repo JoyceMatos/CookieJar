b4e29b05e5eb37d8b463ce5512a1aedd10d3aba9
JoyceMatos <j.matosjoyce@gmail.com>
2016-12-19 11:28:44 -0500

    initial commit

---
+++ CookieJar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CookieJar
+//
+//  Created by Joyce Matos on 12/5/16.
+//  Copyright © 2016 Joyce Matos. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
